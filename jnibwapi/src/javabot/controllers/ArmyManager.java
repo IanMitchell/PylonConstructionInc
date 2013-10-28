3d9f72512a19defad175afb5933ae28eae42e3e4
Alvin Feng <alvin4feng@yahoo.com>
2013-10-27 17:26:46 -0700

    Zealots to squads

--- jnibwapi/src/javabot/controllers/ArmyManager.java
+++ jnibwapi/src/javabot/controllers/ArmyManager.java
@@ -1,11 +1,18 @@
 package javabot.controllers;
+
+import java.util.*;
+
+import javabot.JavaBot;
 import javabot.models.*;
+import javabot.types.UnitType.UnitTypes;
 
 public class ArmyManager implements Manager {
 	private static ArmyManager instance = null;
+	private static ArrayList<Squad> squads;
 	
 	private ArmyManager() {
-		
+		squads = new ArrayList<Squad>();
+		squads.add(new Squad());
 	}
 	
 	public static ArmyManager getInstance() {
@@ -16,18 +23,24 @@ public class ArmyManager implements Manager {
 	}
 	
 	public static void assignUnit(Unit unit) {
-		
+		Squad squad = squads.get(0);
+		squad.assignUnit(unit);
 	}
 
-	@Override
+
 	public void act() {
-		// TODO Auto-generated method stub
-		
+
 	}
 
-	@Override
+
 	public void gameUpdate() {
-		// TODO Auto-generated method stub
-		
+		for(Unit unit : JavaBot.bwapi.getMyUnits()) {
+			if(unit.getTypeID() == UnitTypes.Protoss_Zealot.ordinal()) {
+				assignUnit(unit);
+			}
+		}
+		for(Squad squad : squads) {
+			squad.update();
+		}
 	}
 }
