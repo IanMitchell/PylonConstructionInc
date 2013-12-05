2c131a2c4a296c3ac4d51e3c1561d0a05a77f880
Ian Mitchell <ian.mitchel1@live.com>
2013-12-04 18:32:35 -0800

    Nevermind
    Made a mistake. Should be implemented on top of it.

--- jnibwapi/src/javabot/controllers/TrashManager.java
+++ jnibwapi/src/javabot/controllers/TrashManager.java
@@ -1,23 +1,56 @@
 package javabot.controllers;
 
+
 import javabot.models.Unit;
 
-public class TrashManager {
-	private static TrashManager instance = null;
-	
-	private TrashManager() {
-		
-	}
-	
-	public static TrashManager getInstance() {
-		if(instance == null) {
-			instance = new TrashManager();
-		}
-		return instance;
-	}
-	
-	public void printMessage(String msg) {
-		JavaBot.bwapi.printText(msg);
-	}
+
+public class TrashManager implements Manager {
+        private static TrashManager instance = null;
+        
+        private int actCount = 0;
+        
+        private TrashManager() {
+                
+        }
+        
+        public static TrashManager getInstance() {
+                if(instance == null) {
+                        instance = new TrashManager();
+                }
+                return instance;
+        }
+
+
+        @Override
+        public void act() {
+                // TODO Auto-generated method stub
+                actCount++;
+        }
+
+
+        @Override
+        public void gameUpdate() {
+                // TODO Auto-generated method stub
+                
+        }
+
+
+        @Override
+        public void assignUnit(Unit unit) {
+                // TODO Auto-generated method stub
+                
+        }
+
+
+        @Override
+        public int removeUnit(int unitId) {
+                // TODO Auto-generated method stub
+                return -1;
+        }
+        
+        public void printMessage(String msg) {
+       		JavaBot.bwapi.printText(msg);
+        }
+
 
 }
