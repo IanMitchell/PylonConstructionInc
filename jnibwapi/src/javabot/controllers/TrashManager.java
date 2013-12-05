4a0817f583537fd9dd2d996e9bff624b3eca34e7
Ian Mitchell <ian.mitchel1@live.com>
2013-12-04 18:37:08 -0800

    Initial Trash Talk
    Untested

--- jnibwapi/src/javabot/controllers/TrashManager.java
+++ jnibwapi/src/javabot/controllers/TrashManager.java
@@ -8,6 +8,14 @@ public class TrashManager implements Manager {
         private static TrashManager instance = null;
         
         private int actCount = 0;
+        private int msgCount = 5;
+        
+        private String[] messages = new String[msgCount];
+        messages[0] = "Sup Nick.";
+        messages[1] = "Ready for some embarassment?";
+        messages[2] = "We're gonna send you to the stone age.";
+        messages[3] = "Yo Mama so far she can turn a Creep Colony into a Sunken Colony.";
+        messages[4] = "Go step on a lego.";
         
         private TrashManager() {
                 
@@ -25,6 +33,13 @@ public class TrashManager implements Manager {
         public void act() {
                 // TODO Auto-generated method stub
                 actCount++;
+                
+                if ((actCount % 5) == 0) {
+                        int msg = actCount / 5;
+                        if (msg < msgCount) {
+                                printMessage(messages[msg]);
+                        }
+                }
         }
 
 
