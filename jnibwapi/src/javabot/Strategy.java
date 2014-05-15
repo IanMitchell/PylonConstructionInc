85935f335ee591a0e7a825ed63924734c5736495
ejohns32 <ejohns32@calpoly.edu>
2014-05-14 18:47:22 -0700

    added support for count and type in strategy json
    also refactored "time" to "supplyNum" inside json files

--- jnibwapi/src/javabot/Strategy.java
+++ jnibwapi/src/javabot/Strategy.java
@@ -12,6 +12,7 @@ import java.util.Random;
 import javabot.models.BuildTime;
 import javabot.models.UpgradeBuild;
 import javabot.types.UnitType.UnitTypes;
+import javabot.types.UpgradeType.UpgradeTypes;
 import javabot.Strategy;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -75,6 +76,8 @@ public class Strategy {
 			JavaBot.bwapi.printText("I broke");
 			e.printStackTrace();
 		}
+		
+		printStrategy();
 	}
 	
 	public void loadStrategy(String strategy) throws JsonProcessingException, IOException {
@@ -83,19 +86,59 @@ public class Strategy {
 		
 		for (JsonNode element : rootNode.path("initialPriorityList")) {
 			ObjectNode priority = (ObjectNode)element;
-			initialPriorityList.add(new BuildTime(priority.path("time").intValue(), UnitTypes.valueOf(priority.path("name").textValue())));
+			String type = priority.path("type").textValue();
+			if (type == null) {
+				initialPriorityList.add(new BuildTime(priority.path("supplyNum").asInt(),
+						UnitTypes.valueOf(priority.path("name").textValue())));
+			} else if (type.equals("upgrade")) {
+				initialPriorityList.add(new BuildTime(priority.path("supplyNum").asInt(),
+						new UpgradeBuild(UpgradeTypes.valueOf(priority.path("name").textValue()),
+								UnitTypes.valueOf(priority.path("building").textValue()))));
+			} else {
+				System.out.printf("Found unknown 'type':'%s' in initialPriorityList for %s.json\n", type, strategy);
+			}
+			
 		}
 		
 		for (JsonNode element : rootNode.path("unitPriorityList")) {
 			ObjectNode priority = (ObjectNode)element;
-			unitPriorityList.add(UnitTypes.valueOf(priority.path("name").textValue()));
+			int count = priority.path("count").asInt() != 0 ? priority.path("count").asInt() : 1;
+			
+			for (int i = 0; i < count; i++) {
+				unitPriorityList.add(UnitTypes.valueOf(priority.path("name").textValue()));
+			}
 		}
 		
 		for (JsonNode element : rootNode.path("buildingPriorityList")) {
 			ObjectNode priority = (ObjectNode)element;
-			buildingPriorityList.add(UnitTypes.valueOf(priority.path("name").textValue()));
+			int count = priority.path("count").asInt() != 0 ? priority.path("count").asInt() : 1;
+			
+			for (int i = 0; i < count; i++) {
+				buildingPriorityList.add(UnitTypes.valueOf(priority.path("name").textValue()));
+			}
 		}
 	}
 	
+	private void printStrategy()
+	{
+		System.out.println("\tinitialPriorityList:");
+		for (BuildTime bt : initialPriorityList) {
+			if (bt.getUnit() != null) {
+				System.out.printf("%d : %s\n", bt.getSupplyNum(), bt.getUnit());
+			} else if (bt.getUpgrade() != null) {
+				System.out.printf("%d : %s @ %s\n", bt.getSupplyNum(), bt.getUpgrade().getUpgrade(), bt.getUpgrade().getBuilding());
+			}
+		}
+
+		System.out.println("\tunitPriorityList:");
+		for (UnitTypes ut : unitPriorityList) {
+			System.out.printf("%s\n", ut);
+		}
+		
+		System.out.println("\tbuildingPriorityList:");
+		for (UnitTypes ut : buildingPriorityList) {
+			System.out.printf("%s\n", ut);
+		}
+	}
 
 }
